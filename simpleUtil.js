2a6ce1b4df7e219e206c9e89ac6a2ead22b667d5
wangxueli <1569025161@qq.com>
2017-04-06 21:39:57 +0800

    changewwwww

--- simpleUtil.js
+++ simpleUtil.js
@@ -5,40 +5,36 @@
     var web = 'https://bingtanggithub.github.io/'; 
     var domainId;  
     var idname = [
-                    {
-                        domain: 'bingtanggithub.github.io',
-                        id: '34768c7d19ded3468fdbe770ecddf230'
-                    }, //我的博客
-                    {
-                        domain: 'user.qzone.qq.com/1569025161/infocenter',
-                        id: '593440c1b190e9b67f240ce3e1ee537c'
-                    }, //qq空间
-                    {
-                        domain: 'sjyx.xinguang.com',
-                        id: '9bb005298bfaa0db19d6b3e84688da74'
-                    } //四季严选
-                ];
+        {
+            domain: 'bingtanggithub.github.io',
+            id: '34768c7d19ded3468fdbe770ecddf230'
+        }, //我的博客
+        {
+            domain: 'user.qzone.qq.com/1569025161/infocenter',
+            id: '593440c1b190e9b67f240ce3e1ee537c'
+        }, //qq空间
+        {
+            domain: 'sjyx.xinguang.com',
+            id: '9bb005298bfaa0db19d6b3e84688da74'
+        } //四季严选
+    ];
 
     for (let i=0; i<idname.length; i++) {
             if (web.indexOf(idname[i].domain) != -1) {
                 domainId = idname[i].id;
             }
     }
-console.log('domainId:',domainId);
+
+    console.log('domainId:',domainId);
 
     (function() {
           var _hmt = _hmt || [];
-          // var domainId = simpleUtil('https://bingtanggithub.github.io/');
           var hm = document.createElement("script");
-          // hm.id = "baiDuTongJi";
           hm.src = "https://hm.baidu.com/hm.js?" + domainId;
           var s = document.getElementsByTagName("script")[0]; 
           s.parentNode.insertBefore(hm, s);
-  console.log("1222333");
-  // console.log('domainId:',domainId);
-  // console.log(document.getElementById("baiDuTongJi"));
-})();
-
+        console.log("1222333");
+ 
+    })();
     
-    // return domainId;
 })()
